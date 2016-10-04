94405d718d5e4a831dca3bf58c1aececcec7155f
Raymond Hammarling <raykay.rh@gmail.com>
2016-10-05 00:34:54 +0100

    Integrate Deepstream song-state record more properly with player

--- src/deepstream.js
+++ src/deepstream.js
@@ -1,9 +1,11 @@
 import minimist from 'minimist';
 import deepstream from 'deepstream.io-client-js';
 import R from 'ramda';
-import _ from 'lodash';
 import Radio from './radio';
-import Queue from './queue';
+
+function modifyRecord(record, path, fn) {
+    record.set(path, fn(record.get(path)));
+}
 
 async function main(argv) {
     const dsClient = deepstream(process.env.DEEPSTREAM_HOST_PORT).login({
@@ -16,34 +18,51 @@ async function main(argv) {
     });
     radio.out.pipe(process.stdout);
 
-    const queue = new Queue(radio);
-
-    dsClient.event.subscribe('queue', data => {
-        switch(data.action) {
-            case 'add': queue.queueUrl(data.url); break;
-            case 'next': queue.next(); break;
-        }
-    });
-
     const songStateRecord = dsClient.record.getRecord('song-state');
-    const setSongStatePath = R.curryN(2, ::songStateRecord.set);
+
+    let currentlyPlayingUrl = null;
 
     songStateRecord.whenReady(() => {
-        const data = songStateRecord.get();
+        songStateRecord.set({ ...songStateRecord.get(), currentSong: null, queue: [] });
 
-        // if(data.currentSong != null) {
-        //     queue.queueUrl(data.currentSong);
-        // }
+        dsClient.event.subscribe('queue', data => {
+            switch(data.action) {
+                case 'add': {
+                    modifyRecord(songStateRecord, 'queue', R.append(data.url));
+                    break;
+                }
+                case 'next': {
+                    radio.stop();
+                    break;
+                }
+            }
+        });
 
-        if(_.isArray(data.queue)) {
-            data.queue.forEach(::queue.queueUrl);
-        }
+        // Manage popping off of queue if nothing's playing ATM
+        songStateRecord.subscribe(({ currentSong, queue }) => {
+            if(currentSong == null && !R.isEmpty(queue)) {
+                songStateRecord.set('currentSong', R.head(queue));
+                songStateRecord.set('queue', R.tail(queue));
+            }
+        }, true);
 
-        setSongStatePath({ currentSong: null, queue: queue.queue, ...data });
+        // Manage always playing currentSong
+        songStateRecord.subscribe('currentSong', currentSong => {
+            if(currentlyPlayingUrl == currentSong) return;
+            currentlyPlayingUrl = currentSong;
 
-        queue.on('queue-changed', setSongStatePath('queue'));
-        radio.on('song-end', () => setSongStatePath('currentSong', null));
-        radio.on('song-start', setSongStatePath('currentSong'));
+            radio.stop();
+
+            if(currentSong != null) {
+                radio.play(currentSong);
+            }
+        }, true);
+
+        // Update currentSong to reflect no song is playing anymore
+        radio.on('song-end', () => {
+            currentlyPlayingUrl = null;
+            songStateRecord.set('currentSong', null);
+        });
 
         radio.on('song-end', (url, stream, manual) => console.error('SONG ENDED:', url, 'MANUALLY?', manual));
         radio.on('song-start', (url) => console.error('NOW PLAYING:', url));
